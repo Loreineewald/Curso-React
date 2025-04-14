94a7f2bad921dc3b2866711dc81120c84f49eeaf
Loreine Ewald <164911094+Loreineewald@users.noreply.github.com>
2025-04-14 11:47:32 -0300

    Utilizando React fragments

--- meu-projeto/src/App.js
+++ meu-projeto/src/App.js
@@ -2,6 +2,8 @@ import './App.css'
 import SayMyName from './components/SayMyName'
 import Pessoa from './components/Pessoa'
 import Frase from './components/Frase'
+import List from './components/List'
+
 
 function App() {
 
@@ -21,6 +23,7 @@ function App() {
         profissao= "Programador" 
         foto="https://via.placeholder.com/150" 
         />
+        <List />
     </div>
   );
 }
